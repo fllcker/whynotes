f50657c9336cfd11fab3381af287103d6252cbc2
fllcker <96314993+jxwide@users.noreply.github.com>
2023-01-14 19:42:10 +0300

    initializing

---
+++ src/main/java/ru/fllcker/whynotes/WhynotesApplication.java
@@ -0,0 +1,13 @@
+package ru.fllcker.whynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhynotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhynotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fllcker/whynotes/WhynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fllcker.whynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhynotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
