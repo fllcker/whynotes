5db0bbe2cd737b923cce607ebc553039ffd5ce4c
fllcker <nefllcker@gmail.com>
2023-01-22 17:56:22 +0300

    + exception handlers

--- src/main/java/ru/fllcker/whynotes/controllers/AuthController.java
+++ src/main/java/ru/fllcker/whynotes/controllers/AuthController.java
@@ -1,11 +1,13 @@
 package ru.fllcker.whynotes.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import ru.fllcker.whynotes.security.JwtRequest;
 import ru.fllcker.whynotes.security.JwtResponse;
 import ru.fllcker.whynotes.services.AuthService;
@@ -17,13 +19,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("login")
-    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest jwtRequest) throws Exception {
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest jwtRequest) {
         JwtResponse tokens = authService.login(jwtRequest);
         return ResponseEntity.ok(tokens);
     }
 
     @PostMapping("signup")
-    public ResponseEntity<JwtResponse> signup(@RequestBody JwtRequest jwtRequest) throws Exception {
+    public ResponseEntity<JwtResponse> signup(@RequestBody JwtRequest jwtRequest) {
         JwtResponse tokens = authService.signup(jwtRequest);
         return ResponseEntity.ok(tokens);
     }
--- src/main/java/ru/fllcker/whynotes/services/AuthService.java
+++ src/main/java/ru/fllcker/whynotes/services/AuthService.java
@@ -1,9 +1,11 @@
 package ru.fllcker.whynotes.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import ru.fllcker.whynotes.models.User;
 import ru.fllcker.whynotes.security.JwtAuthentication;
 import ru.fllcker.whynotes.security.JwtRequest;
@@ -16,20 +18,20 @@ public class AuthService {
     private final JwtProvider jwtProvider;
     private final PasswordEncoder encoder;
 
-    public JwtResponse login(JwtRequest jwtRequest) throws Exception {
+    public JwtResponse login(JwtRequest jwtRequest) {
         User user = usersService.findByEmail(jwtRequest.getEmail())
-                .orElseThrow(() -> new Exception("User not found!"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
 
         if (!encoder.matches(jwtRequest.getPassword(), user.getPasswordHashed()))
-            throw new Exception("Wrong data!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong data");
 
         String accessToken = jwtProvider.generateAccessToken(user);
         return new JwtResponse(accessToken);
     }
 
-    public JwtResponse signup(JwtRequest jwtRequest) throws Exception {
+    public JwtResponse signup(JwtRequest jwtRequest) {
         if (usersService.existsByEmail(jwtRequest.getEmail()).orElse(true))
-            throw new Exception("User with this email already exists!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User with this email already exists!");
 
         User user = new User(jwtRequest.getEmail(), encoder.encode(jwtRequest.getPassword()));
         usersService.create(user);
